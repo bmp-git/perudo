a5e229afaddc0307d1aa57129400e58d64479a68
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2017-02-15 16:55:22 +0100

    Add first implementation of two JFrame. Add a main class. Add res folder with application logo.

---
+++ src/perudo/view/Application.java
@@ -0,0 +1,12 @@
+package perudo.view;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Startpage start = new Startpage();
+        start.showFrame();
+        Lobbypage lobby = new Lobbypage();
+        lobby.showFrame();
+    }
+
+}
