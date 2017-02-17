af897bf5761172ddc4f9f7b79a655bab92030db0
Panchhh <emanuelepancisi@gmail.com>
2017-02-17 19:04:15 +0100

    Rework interface View.java with group.

--- src/perudo/view/View.java
+++ src/perudo/view/View.java
@@ -4,167 +4,163 @@ import java.util.Set;
 import perudo.model.*;
 import perudo.utility.ErrorType;
 import perudo.utility.Response;
-import perudo.utility.Result;
 
 public interface View {
 
-    /*** SETUP ***/
-    /**
-     * Tells the view that the new user has been initialized.
-     * 
-     * @param user
-     *            the user initialized
-     */
-    void initializeNewUserRespond(Response<User> user);
+	/**
+	 * Tells the view that the new user has been initialized.
+	 * 
+	 * @param user
+	 *            the user initialized
+	 */
+	void initializeNewUserRespond(Response<User> user);
 
-    /**
-     * Tells the view that the name of user changed.
-     * 
-     * @param user
-     *            the user whose name has been changed
-     */
-    void changeNameRespond(Response<User> user);
+	/**
+	 * Tells all views that a new user has been initialized.
+	 * 
+	 * @param user
+	 *            the user initialized
+	 */
+	void initializeNewUserNotify(User user);
 
-    /**
-     * Gives to view the list of users.
-     * 
-     * @param users
-     *            the list of users to be given to view
-     */
-    void getUsersRespond(Response<Set<User>> users);
+	/**
+	 * Tells all views that the name of a user has changed.
+	 * 
+	 * @param user
+	 *            the user whose name has been changed
+	 */
+	void changeNameNotify(User user);
 
-    /*** LOBBY ***/
-    // response
-    /**
-     * Tells the view that a new lobby has been created.
-     * 
-     * @param lobby
-     *            the lobby created
-     */
-    void createLobbyRespond(Response<Lobby> lobby);
+	/**
+	 * Gives to view the set of users.
+	 * 
+	 * @param users
+	 *            the set of users to be given to view
+	 */
+	void getUsersRespond(Response<Set<User>> users);
 
-    /**
-     * Gives to view the list of lobbies.
-     * 
-     * @param lobbies
-     *            the list of lobbies to be given to view
-     */
-    void getLobbiesRespond(Response<Set<Lobby>> lobbies);
+	/**
+	 * Tells all views that a new lobby has been created.
+	 * 
+	 * @param lobby
+	 *            the lobby created
+	 */
+	void createLobbyNotify(Lobby lobby);
 
-    /**
-     * Tells the view that a lobby has been joined.
-     * 
-     * @param lobby
-     *            the lobby joined
-     */
-    void joinLobbyRespond(Response<Lobby> lobby);
+	/**
+	 * Gives to view the list of lobbies.
+	 * 
+	 * @param lobbies
+	 *            the list of lobbies to be given to view
+	 */
+	void getLobbiesRespond(Response<Set<Lobby>> lobbies);
 
-    /**
-     * Tells the view that the lobby has been exited.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void exitLobbyRespond(Result response);
+	/**
+	 * Tells all views that a user has joined the lobby.
+	 * 
+	 * @param lobby
+	 *            the lobby joined
+	 * @param user
+	 *            the user joining
+	 */
+	void joinLobbyNotify(Lobby lobby, User user);
 
-    /**
-     * Tells the view that the lobby has started.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void startLobbyRespond(Result response);
+	/**
+	 * Tells all views that a user has left the lobby.
+	 * 
+	 * @param lobby
+	 *            the lobby exited
+	 * @param user
+	 *            the user exiting
+	 */
+	void exitLobbyNotify(Lobby lobby, User user);
 
-    /*** GAME ***/
-    // response
-    /**
-     * Gives to view the list of games.
-     * 
-     * @param games
-     *            the list of games to be given to view
-     */
-    void getGamesRespond(Response<Set<Game>> games);
+	/**
+	 * Tells all views that a lobby turned in a game.
+	 * 
+	 * @param lobby
+	 *            the lobby started
+	 * @param game
+	 *            the game began
+	 */
+	void startLobbyNotify(Lobby lobby, Game game);
 
-    /**
-     * Tells the view that the play has been made or an error occured.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void playRespond(Result response);
+	/**
+	 * Gives to view the list of games.
+	 * 
+	 * @param games
+	 *            the list of games to be given to view
+	 */
+	void getGamesRespond(Response<Set<Game>> games);
 
-    /**
-     * Tells the view that the doubt has been made or an error occured.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void doubtRespond(Result response);
+	/**
+	 * Tells all views that a user has made a play.
+	 * 
+	 * @param game
+	 *            the game with the bid made
+	 * @param user
+	 *            the user who has played
+	 */
+	void playNotify(Game game, User user);
 
-    /**
-     * Tells the view that the urge has been made or an error occured.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void urgeRespond(Result response);
+	/**
+	 * Tells all views that someone has made a doubt.
+	 * 
+	 * @param game
+	 *            the updated game
+	 * @param user
+	 *            the user who has doubted
+	 * @param win
+	 *            the doubt result
+	 */
+	void doubtNotify(Game game, User user, boolean win);
 
-    /**
-     * Tells the view that the palifico turn has been called or an error
-     * occured.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void callPalificoRespond(Result response);
+	/**
+	 * Tells all views that someone has made a urge.
+	 * 
+	 * @param game
+	 *            the updated game
+	 * @param user
+	 *            the user who has urged
+	 * @param win
+	 *            the urge result
+	 */
+	void urgeNotify(Game game, User user, boolean win);
 
-    /**
-     * Tells the view that the game has been exited.
-     * 
-     * @param response
-     *            result of operation
-     */
-    void exitGameRespond(Result response);
+	/**
+	 * Tells all views that a palifico turn has been called.
+	 * 
+	 * @param game
+	 *            the updated game
+	 * @param user
+	 *            the user who has called palifico
+	 */
+	void callPalificoNotify(Game game, User user);
 
-    // notify
-    /**
-     * Tells the view that a game has started.
-     * 
-     * @param game
-     *            result of operation
-     */
-    void gameStarted(Game game);
+	/**
+	 * Tells all views that the user exited the game.
+	 * 
+	 * @param game
+	 *            the game exited
+	 * @param user
+	 *            the user who exited
+	 */
+	void exitGameNotify(Game game, User user);
 
-    /**
-     * Tells the view that a game changed.
-     * 
-     * @param game
-     *            the changed game
-     */
-    void gameChanged(Game game);
+	/**
+	 * Tells all views that the game ended.
+	 * 
+	 * @param game
+	 *            the ended game
+	 */
+	void gameEndedNotify(Game game);
 
-    /**
-     * Tells the view that a game ended.
-     * 
-     * @param game
-     *            the ended game
-     */
-    void gameEnded(Game game);
-
-    /**
-     * Tells the view that an error occurred.
-     * 
-     * @param errorType
-     *            the error type occurred
-     */
-    void showError(ErrorType errorType);
-
-    /**
-     * Tells the view that a lobby changed.
-     * 
-     * @param lobby
-     *            the lobby changed
-     */
-    void lobbyChanged(Lobby lobby);
+	/**
+	 * Tells the view that an error occurred.
+	 * 
+	 * @param errorType
+	 *            the error type occurred
+	 */
+	void showError(ErrorType errorType);
 
 }
