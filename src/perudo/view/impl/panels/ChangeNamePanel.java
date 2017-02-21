04a7a3d8a34bb03e3adb3c80a6e423ca951122e6
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2017-02-21 16:36:45 +0100

    Add ChangeNamePanel.java MenuBottomPanel.java.

---
+++ src/perudo/view/impl/panels/ChangeNamePanel.java
@@ -0,0 +1,38 @@
+package perudo.view.impl.panels;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import perudo.view.GUIFactory;
+import perudo.view.impl.StandardGUIFactory;
+
+public class ChangeNamePanel extends JPanel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String LABEL_NAME = "New name";
+    private final GUIFactory factory;
+    private JTextField txfName;
+    
+    public ChangeNamePanel() {
+        super();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.factory = new StandardGUIFactory();
+        this.txfName = (JTextField) this.factory.createTextField();
+        
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_NAME));
+        this.add(this.txfName);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+    }
+    
+    public String getName() {
+        return this.txfName.getText();
+    }
+}
---
+++ src/perudo/view/impl/panels/MenuBottomPanel.java
@@ -0,0 +1,66 @@
+package perudo.view.impl.panels;
+
+import java.awt.BorderLayout;
+import java.util.Optional;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import perudo.model.User;
+import perudo.view.GUIFactory;
+import perudo.view.impl.ControllerSingleton;
+import perudo.view.impl.StandardGUIFactory;
+
+public class MenuBottomPanel extends JPanel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private final GUIFactory factory;
+    private Optional<User> user;
+    private JLabel lblname;
+    private JLabel lblonline;
+
+    public MenuBottomPanel() {
+        super();
+        this.factory = new StandardGUIFactory();
+        this.user = Optional.empty();
+        this.setBorder(new EmptyBorder(5, 10, 5, 10));
+
+        if (this.user.isPresent()) {
+            if (ControllerSingleton.getController().getClass().getName().equals("StandardControllerImpl")) {
+                this.lblname = (JLabel) this.factory.createLabel("Logged as " + this.user.get().getName());
+                this.lblonline = (JLabel) this.factory.createLabel("Online.");
+            } else {
+                this.lblname = (JLabel) this.factory.createLabel("Logged as " + this.user.get().getName());
+                this.lblonline = (JLabel) this.factory.createLabel("Offline singleplayer.");
+            }
+        } else {
+            this.lblname = (JLabel) this.factory.createLabel("Not logged yet..");
+            this.lblonline = (JLabel) this.factory.createLabel("Offline.");
+        }
+        this.setLayout(new BorderLayout());
+        this.add(this.lblname, BorderLayout.LINE_START);
+        this.add(this.lblonline, BorderLayout.LINE_END);
+    }
+
+    public void setUser(User user) {
+        this.user = Optional.ofNullable(user);
+        if (this.user.isPresent()) {
+            if (ControllerSingleton.getController().getClass().getName().equals("perudo.controller.impl.NetworkControllerImpl")) {
+                System.out.println(ControllerSingleton.getController().getClass().getName());
+                this.lblname.setText("Logged as " + this.user.get().getName());
+                this.lblonline.setText("Online.");
+            } else {
+                this.lblname.setText("Logged as " + this.user.get().getName());
+                this.lblonline.setText(("Offline singleplayer."));
+            }
+        } else {
+            this.lblname.setText("Not logged yet..");
+            this.lblonline.setText("Offline.");
+        }
+    }
+
+}
