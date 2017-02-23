8e6e1b7ebb916b243dab24aa4736d96c97437f36
Lorenzo Mondani <lorenzo.mondani@studio.unibo.it>
2017-02-23 15:14:19 +0100

    Add Closeable interface to DatagramStream.java

--- src/perudo/controller/net/DatagramStream.java
+++ src/perudo/controller/net/DatagramStream.java
@@ -1,8 +1,9 @@
 package perudo.controller.net;
 
+import java.io.Closeable;
 import java.util.function.BiConsumer;
 
-public interface DatagramStream {
+public interface DatagramStream extends Closeable {
 	void send(Datagram datagram);
 
 	void onDatagramReceived(BiConsumer<Datagram, DatagramStream> consumer);
