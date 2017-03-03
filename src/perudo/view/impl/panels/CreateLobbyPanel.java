8f26b248c263da8564278ee8969d21a09f974720
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2017-03-03 19:35:22 +0100

    Fix CreateLobbyPanel.java default parameters.

--- src/perudo/view/impl/panels/CreateLobbyPanel.java
+++ src/perudo/view/impl/panels/CreateLobbyPanel.java
@@ -17,84 +17,94 @@ import perudo.view.impl.StandardGUIFactory;
 
 public class CreateLobbyPanel extends JPanel {
 
-        /**
-         * 
-         */
-        private static final long serialVersionUID = 1L;
-        public final static String TITLE = "Create a new lobby...";
-        public final static String ICON_RESPATH = "/images/perudo-logo.png";
-        private final static String LABEL_NAME = "Name";
-        private final static String LABEL_PLAYERS_NUM = "Max players";
-        private final static String LABEL_DICES_NUM = "Max dices";
-        private final static String LABEL_DICES_VALUE = "Max dices value";
-        private final static String LABEL_TURN_TIME = "Turn time in seconds";
-        private final static int TURN_TIME_LABEL_TICK = 117;
-        
-        private final static Object[] N_PLAYERS = IntStream
-                        .rangeClosed(GameSettingsImpl.MIN_PLAYER_NUMBERS, GameSettingsImpl.MAX_PLAYER_NUMBERS)
-                        .mapToObj(e -> String.valueOf(e)).toArray();
-        private final static Object[] N_DICE = IntStream
-                        .rangeClosed(GameSettingsImpl.MIN_INITIAL_DICE_NUMBERS, GameSettingsImpl.MAX_INITIAL_DICE_NUMBERS)
-                        .mapToObj(e -> String.valueOf(e)).toArray();
-        private final static Object[] DICES_VALUE = IntStream
-                        .rangeClosed(GameSettingsImpl.MIN_DICE_FACES, GameSettingsImpl.MAX_DICE_FACES)
-                        .mapToObj(e -> String.valueOf(e)).toArray();
-        
-        private final GUIFactory factory;
-        private JTextField txfName;
-        private JComboBox<Object> cmbPlayers;
-        private JComboBox<Object> cmbDices;
-        private JComboBox<Object> cmbDicesValue;
-        private JSlider sldTime;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    public final static String TITLE = "Create a new lobby...";
+    public final static String ICON_RESPATH = "/images/perudo-logo.png";
+    private final static String LABEL_NAME = "Name";
+    private final static String LABEL_PLAYERS_NUM = "Max players";
+    private final static String LABEL_DICES_NUM = "Max dices";
+    private final static String LABEL_DICES_VALUE = "Max dices value";
+    private final static String LABEL_TURN_TIME = "Turn time in seconds";
+    private final static int TURN_TIME_LABEL_TICK = 117;
+    private final static int TURN_TIME_DEF = 60;
+    private final static int DICE_NUM_DEF = 5 - GameSettingsImpl.MIN_INITIAL_DICE_NUMBERS;
+    private final static int DICE_VALUE_DEF = 6 - GameSettingsImpl.MIN_DICE_FACES;
+    private final static int PLAYER_NUM_DEF = 4 - GameSettingsImpl.MIN_PLAYER_NUMBERS;
 
-        public CreateLobbyPanel() {
-                this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-                this.factory = new StandardGUIFactory();
-                this.txfName = (JTextField) this.factory.createTextField();
-                this.cmbPlayers = (JComboBox<Object>) this.factory.createComboBox(N_PLAYERS);
-                this.cmbDices = (JComboBox<Object>) this.factory.createComboBox(N_DICE);
-                this.cmbDicesValue = (JComboBox<Object>) this.factory.createComboBox(DICES_VALUE);
-                int middle = ((int)GameSettingsImpl.MAX_TURN_TIME.getSeconds() - (int)GameSettingsImpl.MIN_TURN_TIME.getSeconds()) /2;
-                this.sldTime = (JSlider) this.factory.createSliderHorizontal((int)GameSettingsImpl.MIN_TURN_TIME.getSeconds(), (int)GameSettingsImpl.MAX_TURN_TIME.getSeconds(), middle);
-                this.sldTime.setMajorTickSpacing(TURN_TIME_LABEL_TICK);
-                this.sldTime.setPaintLabels(true);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-                this.add(this.factory.createLabel(LABEL_NAME));
-                this.add(this.txfName);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-                this.add(this.factory.createLabel(LABEL_PLAYERS_NUM));
-                this.add(this.cmbPlayers);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-                this.add(this.factory.createLabel(LABEL_DICES_NUM));
-                this.add(this.cmbDices);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-                this.add(this.factory.createLabel(LABEL_DICES_VALUE));
-                this.add(this.cmbDicesValue);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-                this.add(this.factory.createLabel(LABEL_TURN_TIME));
-                this.add(this.sldTime);
-                this.add(Box.createRigidArea(new Dimension(0, 10)));
-        }
+    private final static Object[] N_PLAYERS = IntStream
+            .rangeClosed(GameSettingsImpl.MIN_PLAYER_NUMBERS, GameSettingsImpl.MAX_PLAYER_NUMBERS)
+            .mapToObj(e -> String.valueOf(e)).toArray();
+    private final static Object[] N_DICE = IntStream
+            .rangeClosed(GameSettingsImpl.MIN_INITIAL_DICE_NUMBERS, GameSettingsImpl.MAX_INITIAL_DICE_NUMBERS)
+            .mapToObj(e -> String.valueOf(e)).toArray();
+    private final static Object[] DICES_VALUE = IntStream
+            .rangeClosed(GameSettingsImpl.MIN_DICE_FACES, GameSettingsImpl.MAX_DICE_FACES).mapToObj(e -> String.valueOf(e))
+            .toArray();
 
-        public String getName() {
-                return this.txfName.getText();
-        }
-        
-        public int getMaxPlayers() {
-                return Integer.parseInt(this.cmbPlayers.getSelectedItem().toString());
-        }
-        public int getMaxDices() {
-                return Integer.parseInt(this.cmbDices.getSelectedItem().toString());
-        }
-        public int getMaxDicesValue() {
-                return Integer.parseInt(this.cmbDicesValue.getSelectedItem().toString());
-        }
-        
-        public Duration getTurnTime() {
-                return Duration.ofSeconds(this.sldTime.getValue());
-        }
-        
-        public GameSettings getGameSettings() {
-            return new GameSettingsImpl(this.getMaxPlayers(),this.getMaxDicesValue(),this.getMaxDices(),this.getTurnTime(),this.getName());
-        }
+    private final GUIFactory factory;
+    private JTextField txfName;
+    private JComboBox<Object> cmbPlayers;
+    private JComboBox<Object> cmbDices;
+    private JComboBox<Object> cmbDicesValue;
+    private JSlider sldTime;
+
+    public CreateLobbyPanel() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.factory = new StandardGUIFactory();
+        this.txfName = (JTextField) this.factory.createTextField();
+        this.cmbPlayers = (JComboBox<Object>) this.factory.createComboBox(N_PLAYERS);
+        this.cmbPlayers.setSelectedIndex(PLAYER_NUM_DEF);
+        this.cmbDices = (JComboBox<Object>) this.factory.createComboBox(N_DICE);
+        this.cmbDices.setSelectedIndex(DICE_NUM_DEF);
+        this.cmbDicesValue = (JComboBox<Object>) this.factory.createComboBox(DICES_VALUE);
+        this.cmbDicesValue.setSelectedIndex(DICE_VALUE_DEF);
+        this.sldTime = (JSlider) this.factory.createSliderHorizontal((int) GameSettingsImpl.MIN_TURN_TIME.getSeconds(),
+                (int) GameSettingsImpl.MAX_TURN_TIME.getSeconds(), TURN_TIME_DEF);
+        this.sldTime.setMajorTickSpacing(TURN_TIME_LABEL_TICK);
+        this.sldTime.setPaintLabels(true);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_NAME));
+        this.add(this.txfName);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_PLAYERS_NUM));
+        this.add(this.cmbPlayers);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_DICES_NUM));
+        this.add(this.cmbDices);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_DICES_VALUE));
+        this.add(this.cmbDicesValue);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(this.factory.createLabel(LABEL_TURN_TIME));
+        this.add(this.sldTime);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+    }
+
+    public String getName() {
+        return this.txfName.getText();
+    }
+
+    public int getMaxPlayers() {
+        return Integer.parseInt(this.cmbPlayers.getSelectedItem().toString());
+    }
+
+    public int getMaxDices() {
+        return Integer.parseInt(this.cmbDices.getSelectedItem().toString());
+    }
+
+    public int getMaxDicesValue() {
+        return Integer.parseInt(this.cmbDicesValue.getSelectedItem().toString());
+    }
+
+    public Duration getTurnTime() {
+        return Duration.ofSeconds(this.sldTime.getValue());
+    }
+
+    public GameSettings getGameSettings() {
+        return new GameSettingsImpl(this.getMaxPlayers(), this.getMaxDicesValue(), this.getMaxDices(), this.getTurnTime(),
+                this.getName());
+    }
 }
